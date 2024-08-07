256e9209b43d7a18157bb93e4ff6a2d61cd33827
leoramosa <leo.ramos.ari@gmail.com>
2024-08-06 22:26:20 -0300

    como hacer redirects con useRoter y redirect

--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -1,5 +1,6 @@
 import { ProductView } from "app/components/product/ProductView/ProductView";
 import { getProducts } from "app/services/shopify/products";
+// import { redirect } from "next/navigation";
 //usaresmos otro metodo con los hooks
 
 // import { useParams, useSearchParams } from "next/navigation";
@@ -22,5 +23,10 @@ export default async function ProductPage({ searchParams }: ProductPageProps) {
   const id = searchParams.id;
   const products = await getProducts(id);
   const product = products[0];
+  //redirect primer metodo
+  // if (!id) {
+  //   redirect("/store");
+  // }
+
   return <ProductView product={product} />;
 }
--- src/components/product/ProductView/ProductView.tsx
+++ src/components/product/ProductView/ProductView.tsx
@@ -1,11 +1,20 @@
+"use client";
+//redirect como hooks
+
 import Image from "next/image";
 import { ProductViewItemsOrder } from "./ProductViewItemsOrder";
 import styles from "./ProductView.module.sass";
+import { useRouter } from "next/navigation";
 interface ProductViewProps {
   product: ProductType;
 }
 
 export const ProductView = ({ product }: ProductViewProps) => {
+  //segundo metodo de redirecto usando un hooks
+  const router = useRouter();
+  if (!product) {
+    router.push("/");
+  }
   return (
     <main className={styles.ProductView}>
       <section className={styles.ProductView__images}>
